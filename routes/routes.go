06c032394a9a9269666383dae9b75a3a2904607d
Thiago F <thiago.falcao86@gmail.com>
2022-10-10 18:52:17 -0300

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,8 +1,9 @@
 package routes
 
 import (
+	"tfalc/Api-gin/controllers"
+
 	"github.com/gin-gonic/gin"
-	"github.com/tfalc/Api-gin/controllers"
 )
 
 func HandleRequests() {
@@ -10,6 +11,8 @@ func HandleRequests() {
 
 	r.GET("/", controllers.ExiberPaginaInicial)
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	r.GET("/:nome", controllers.Greetings)
+	r.POST("/alunos", controllers.CriaNovoAluno)
 
 	r.Run()
 }
